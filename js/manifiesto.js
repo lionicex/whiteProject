fd859550e8da525685541513a4f779a82788d2c7
lionicex <lionicex@gmail.com>
2018-05-23 10:16:10 +0100

    new email share button

--- js/manifiesto.js
+++ js/manifiesto.js
@@ -1,13 +1,13 @@
 /*MANIFIESTO*/
 let a = document.getElementById("texto");
 let rest = a.innerHTML;
-console.log(rest);
-
 let botonModificar = document.getElementById("modificar");
 let botonGuardar = document.getElementById("guardar");
 let botonRestaurar = document.getElementById("restaurar");
 let botonCompartir = document.getElementById("compartir");
 let botonSocial3 = document.getElementById("social3");
+let autor = document.getElementById("nombre");
+
 
 function modificar() {
     a.contentEditable = "true";
@@ -16,6 +16,10 @@ function modificar() {
     botonGuardar.style.display = "block";
     botonModificar.style.display = "none";
     botonRestaurar.style.display = "block";
+    autor.style.display = "inline-block";
+    console.log(a.innerHTML.length);
+    console.log(rest.length);
+    console.log(a.textContent.length);
 
 }
 
@@ -28,7 +32,12 @@ function guardar() {
     botonRestaurar.style.display = "none";
     botonCompartir.style.display = "block";
     botonSocial3.style.display = "none";
+    if (a.length == rest.lenght) {
+        autor.style.display = "none";
+    }
+
 }
+
 
 function restaurar() {
     a.innerHTML = rest;
@@ -45,4 +54,15 @@ function saveTextAsFile() {
     doc.setFontSize(12);
     doc.text(splitText, 10, 10);
     doc.save('WhiteProject - manifiesto.pdf');
+}
+
+function setEmailHref() {
+    let email = document.getElementById('email-button');
+    email.href = 'mailto:?body=' + encodeURI(a.textContent);
+}
+
+function setTwitterHref() {
+    let twitter = document.getElementById('twitter-button');
+    twitter.href = 'http://twitter.com/share?url=http://wwww.whiteproject.com&hashtags=Manifiesto&text=' +
+        encodeURI(a.textContent);
 }
